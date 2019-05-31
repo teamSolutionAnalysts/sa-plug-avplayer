1069ccc47069f4bc31093f5d5b38352d23ba576f
Paresh Prajapati <paresh.prajapati.sa@gmail.com>
2019-05-31 18:17:09 +0530

    Added Carthage files

---
+++ sa-plug-avplayer/sa_plug_avplayer.h
@@ -0,0 +1,19 @@
+//
+//  sa_plug_avplayer.h
+//  sa-plug-avplayer
+//
+//  Created by Paresh Prajapati on 31/05/19.
+//  Copyright © 2019 Solutionanalysts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for sa_plug_avplayer.
+FOUNDATION_EXPORT double sa_plug_avplayerVersionNumber;
+
+//! Project version string for sa_plug_avplayer.
+FOUNDATION_EXPORT const unsigned char sa_plug_avplayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sa_plug_avplayer/PublicHeader.h>
+
+
